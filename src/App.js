cf76b3777d2205d568bc4352413732a2415ca5e5
anandvardhanmishra <10anandvardhanmishra@gmail.com>
2021-09-14 01:00:13 +0530

    updating App.js

--- src/App.js
+++ src/App.js
@@ -64,7 +64,7 @@ export default function App() {
       count = await bumpportalContract.getTotalBumps();
       console.log("Retrieved total bump count...", count.toNumber());
 
-      setCountBumps(bump);
+      setCountBumps(count);
     }
     
 
