3ecaceabc949bac40a49c6422c7bf2ae9a04902b
anandvardhanmishra <10anandvardhanmishra@gmail.com>
2021-09-13 21:09:56 +0530

    updating App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { ethers } from "ethers";
 import './App.css';
 
 export default function App() {
+  // Just a state variable we use to store our user's public wallet address
+  const[currAccount, setCurrAccount] = React.useState("");
   
   const checkIfWalletIsConnected = () => {
       // First Make Sure we have access to window.ethereum
@@ -13,6 +15,20 @@ export default function App() {
       } else {
         console.log("We have the ethereum object", ethereum);
       }
+      // Check if we're authorized to access the user's wallet
+      ethereum.request({ method: 'eth_accounts' }).then(accounts => {
+        // We could have multiple accounts. Check for one
+        if(accounts.length !== 0) {
+          // Grab the first account we have access to
+          const account = accounts[0];
+          console.log("Found an authorized account: ", account);
+          
+          // Store the user's public wallet address for later
+          setCurrAccount(account);
+        } else {
+          console.log("No authorized account found");
+        }
+      })
     }
     
     // This runs our function when the page loads
